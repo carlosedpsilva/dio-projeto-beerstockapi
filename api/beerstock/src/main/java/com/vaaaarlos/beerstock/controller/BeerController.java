aae516231bb5fa94a01d06e46c7abf5ea396c720
vaaaarlos <kadu.cerq@gmail.com>
2021-06-28 01:01:27 -0300

    feat(beer-controller): :sparkles: Add get all Beers operation

--- api/beerstock/src/main/java/com/vaaaarlos/beerstock/controller/BeerController.java
+++ api/beerstock/src/main/java/com/vaaaarlos/beerstock/controller/BeerController.java
@@ -11,12 +11,16 @@ import com.vaaaarlos.beerstock.exception.BeerNotFoundException;
 import com.vaaaarlos.beerstock.service.BeerService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -47,6 +51,19 @@ public class BeerController implements BeerControllerDocs {
   @ResponseStatus(HttpStatus.OK)
   public BeerResponse findById(@PathVariable long id) throws BeerNotFoundException {
     return beerService.findById(id);
+  }
+
+  @GetMapping
+  @ResponseStatus(HttpStatus.OK)
+  public Page<BeerResponse> findAll(
+    @RequestParam(value =         "page", defaultValue =   "0") Integer page,
+    @RequestParam(value = "linesPerPage", defaultValue =   "8") Integer linesPerPage,
+    @RequestParam(value =    "direction", defaultValue = "ASC")  String direction,
+    @RequestParam(value =      "orderBy", defaultValue =  "id")  String orderBy,
+    @RequestParam(value =         "name", defaultValue =    "")  String name
+  ) {
+    var pageRequest = PageRequest.of(0, 8, Direction.valueOf(direction), orderBy);
+    return beerService.findAll(pageRequest, name);
   }
 
 }
